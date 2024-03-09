0b2ff0a8df1dfdf2cd9a947bd670282e72ea2b15
Mohammed Ali Baddah <mabaddah@gmail.com>
2024-03-10 06:44:48 +1100

    Saving progress

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 #Simple springboot app
 
 - Run with `mvn clean spring-boot:run`
+
+Docs:
 - View docs via http://localhost:8080/v3/api-docs
 - View swagger docs via http://localhost:8080/swagger-ui/index.html
-- Access via http://localhost:8080/
 
-Wip:
-- MondoDB
+Usage:
+- Access via http://localhost:8080/
+- Post : localhost:8080/items + payload
+- Put : e.g. localhost:8080/items?id=Dried%20Red%20Chilli + payload
+- Delete : localhost:8080/items?id=Dried%20Red%20Chilli
 
 To-do:
 - Dockerise
--- src/main/java/org/example/GroceryItemController.java
+++ src/main/java/org/example/GroceryItemController.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,5 +35,33 @@ public class GroceryItemController {
         return groceryItemRepo.findAll(category);
     }
 
+
+    @PostMapping
+    public ResponseEntity<String> createItem(@RequestBody GroceryItem item) {
+        groceryItemRepo.save(item);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Item created successfully");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateItem(@RequestParam("id") String id, @RequestBody GroceryItem updatedItem) {
+        if (groceryItemRepo.existsById(id)) {
+            updatedItem.setId(id);
+            groceryItemRepo.save(updatedItem);
+            return ResponseEntity.ok("Item updated successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+// Example usage localhost:8080/items?id=Dried%20Red%20Chilli
+    @DeleteMapping()
+    public ResponseEntity<String> deleteItem(@RequestParam("id") String id) {
+        if(groceryItemRepo.existsById(id)) {
+            groceryItemRepo.deleteById(id);
+            return ResponseEntity.ok("Item successfully deleted");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
